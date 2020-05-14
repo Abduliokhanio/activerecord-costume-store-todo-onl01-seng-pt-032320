65a5769fcf90c330919967caea6147a33c02bdeb
Abdul Khan <akhan17@outlook.com>
2020-05-14 20:10:55 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -12,6 +12,9 @@ class CreateCostumes < ActiveRecord::Migration[5.1]
   def change
     create_table :costume_stores do |t|
       t.string :name
+      t.integer :price
+      t.string :image_url
+      t.string :size
       t.timestamps
     end
   end
