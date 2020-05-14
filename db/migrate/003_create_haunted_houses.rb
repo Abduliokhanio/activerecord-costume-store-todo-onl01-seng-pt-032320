4861f62fcc29043df9e208c4fa600eb209c5dc21
Abdul Khan <akhan17@outlook.com>
2020-05-14 20:26:35 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -2,4 +2,14 @@
 
 class CreateHauntedHouses < ActiveRecord::Migration[5.1]
 
+  def change
+    create_table :haunted_houses do |t|
+      t.string :name
+      t.string :location
+      t.string :theme
+      t.integer :price
+      t.boolean :family_friendly
+    end
+  end
+
 end
