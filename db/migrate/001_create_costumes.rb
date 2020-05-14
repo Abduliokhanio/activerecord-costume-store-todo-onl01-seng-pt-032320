859b4da913eb8970d2f95a9f4db5f47172e70610
Abdul Khan <akhan17@outlook.com>
2020-05-14 19:36:56 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -9,4 +9,9 @@
 # value types according to the spec
 
 class CreateCostumes < ActiveRecord::Migration[5.1]
+  def change
+    create_table :costumes do |t|
+      t.string :name  
+    end 
+  end 
 end
