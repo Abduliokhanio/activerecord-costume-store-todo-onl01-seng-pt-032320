e5ecad406fbd8f9f70290d0a9cec95dc9bbeb132
Abdul Khan <akhan17@outlook.com>
2020-05-14 20:03:45 +0000

    Automatically backed up by Learn

--- app/models/costume_store.rb
+++ app/models/costume_store.rb
@@ -2,6 +2,11 @@
 
 class CostumeStore < ActiveRecord::Base
   def change
+    create_table :costumes do |t|
+      t.string :name
+      t.integer :price
+      t.string :image_url
+      t.string :size
+      t.timestamps
   end
-
 end
