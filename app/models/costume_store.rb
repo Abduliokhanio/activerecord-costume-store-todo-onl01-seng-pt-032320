adab3cd30a3434a870274fa5e4249115d596b942
Abdul Khan <akhan17@outlook.com>
2020-05-14 20:12:19 +0000

    Automatically backed up by Learn

--- app/models/costume_store.rb
+++ app/models/costume_store.rb
@@ -1,13 +1,5 @@
 # Create your CostumeStore class here
 
 class CostumeStore < ActiveRecord::Base
-  def change
-    create_table :store do |t|
-      t.string :name
-      t.string :location
-      t.string :costume_inventort
-      t.integer :employee_count
-      t.timestamps
-    end
-  end
+
 end
